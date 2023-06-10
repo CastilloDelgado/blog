a958692169ba276e3e63ebadf242d00c0884d03b
Marco Castillo <marcocastillo1997@gmail.com>
2023-06-10 11:50:23 -0600

    Adding Banner 1

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -15,7 +15,7 @@
                 </div>
             </nav>
         </section>
-        <div class="bg-white container mx-auto px-6 py-4">
+        <div class="bg-white container mx-auto py-4">
             <!-- Title -->
             <section class="flex justify-center flex-col py-12">
                 <div class="flex justify-center mb-8">
@@ -42,12 +42,12 @@
                             <option value="">2</option>
                             <option value="">3</option>
                         </select>
-                        <input type="text" class="border-0 bg-gray-200 border-l-4 border-black placeholder-black"  placeholder="Buscar por barda">
+                        <input type="text" class="border-0 bg-gray-200 border-l-4 border-black placeholder-black"  placeholder="Buscar por banda">
                     </form>
                 </div>
             </section>
             <!-- Section - The best -->
-            <div class="px-8">
+            <section class="px-12 pb-12">
                 <div class="mb-4 pb-2 border-b-2 border-black" >
                     <b class="text-xl">Los mejores reviews</b>
                 </div>
@@ -97,7 +97,20 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </section>
+            <!-- Banner 1 -->
+            <section class="flex">
+                <div class="w-3/5 object-fill">
+                    <img src="../../images/banner-1.jpg" alt="" class="grayscale">
+                </div>
+                <div class="w-2/5 bg-black border-l-4 border-white flex justify-center flex-col px-16">
+                    <p class="font-serif text-4xl text-white text-center mb-4">¿Tienes algo que contar?</p>
+                    <p class="font-sans text-xl text-white text-center mb-12">Crea tu cuenta y comienza a escribir reviews de los conciertos a los que has asistido.</p>
+                    <div class="flex justify-center">
+                        <button type="button" class="border-0 bg-gray-300 border-l-4 border-white focus:outline-none mr-2 font-bold px-6 py-2 w-64">Crea tu cuenta</button>
+                    </div>
+                </div>
+            </section>
         </div>
     </div>
 </template>
