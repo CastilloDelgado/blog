cb47fbaf1d2fbd657c170cb4c54ffc630aa52330
Marco Castillo <marcocastillo1997@gmail.com>
2023-06-19 23:07:00 -0600

    Making the post layout reponsive to mobile

--- resources/js/Components/TagBadge.vue
+++ resources/js/Components/TagBadge.vue
@@ -5,7 +5,7 @@
 </script>
 
 <template>
-    <div class="mr-3 shadow-[-4px_4px_0px_0px_rgba(0,0,0,1)] border border-black px-2 hover:bg-black hover:text-white hover:border-l-white hover:border-b-white">
+    <div class="ml-3 shadow-[-4px_4px_0px_0px_rgba(0,0,0,1)] border border-black px-2 hover:bg-black hover:text-white hover:border-l-white hover:border-b-white">
         <p class="text-sm capitalize" >{{ tag.name }}</p> 
     </div>
 </template>
--- resources/js/Pages/Post.vue
+++ resources/js/Pages/Post.vue
@@ -18,10 +18,10 @@ import PostMainImage from '@/Components/PostMainImage.vue';
         <div class="bg-gray-200">
             <div class="bg-white container mx-auto py-4">
                 <!-- Post Layout -->
-                <div class="pt-12 px-32 mb-12">
+                <div class="pt-12 px-6 md:px-12 lg:px-32 mb-12">
                     <div class="flex mb-6">
-                        <div class="w-1/5"></div>
-                        <div class="w-4/5 pl-12 mb-4 flex justify-between">
+                        <div class="hidden lg:flex lg:w-1/5"></div>
+                        <div class="w-full lg:w-4/5 lg:pl-12 mb-4 flex justify-between">
                             <div>
                                 <Link :href="route('home')">
                                     <p class="font-bold text-sm hover:underline">{{ "< Regresar" }}</p>
@@ -33,12 +33,16 @@ import PostMainImage from '@/Components/PostMainImage.vue';
                             </div>
                         </div>
                     </div>
-                    <div class="flex">
-                        <div class="w-1/5">
-                            <PostMainImage :image_url="post.image_url" />
-                            <PostProfileBadge :published_at="post.published_at"  :author="post.author"/>
+                    <div class="flex flex-col lg:flex-row ">
+                        <div class="lg:w-1/5 mb-8 flex flex-row lg:flex-col w-full">
+                            <div class="w-1/2 lg:w-full">
+                                <PostMainImage :image_url="post.image_url" />
+                            </div>
+                            <div class="w-1/2 lg:w-full pl-6  lg:pl-0 self-end lg:self-top">
+                                <PostProfileBadge :published_at="post.published_at"  :author="post.author"/>
+                            </div>
                         </div>
-                        <div class="w-4/5 pl-12">
+                        <div class="lg:w-4/5 lg:pl-12">
                             <div class="mb-4">
                                 <p class="font-serif text-4xl font-bold">{{ post?.title || "No title" }}</p>
                             </div>
@@ -50,7 +54,7 @@ import PostMainImage from '@/Components/PostMainImage.vue';
                                 <p>{{ paragraph.text }}</p>
                             </div>
 
-                            <div class="grid grid-cols-3 gap-4 mb-4">
+                            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
                                 <img v-for="image in post.images" v-bind:src="image.image_url" alt="" class="border-2 border-black">
                             </div>
                         </div>
