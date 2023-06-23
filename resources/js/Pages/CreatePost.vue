620b3ac112f979dedd42fe404d6bc65b64726295
Marco Castillo <marcocastillo1997@gmail.com>
2023-06-22 18:18:38 -0600

    Creating form to create post

--- resources/js/Pages/CreatePost.vue
+++ resources/js/Pages/CreatePost.vue
@@ -3,7 +3,17 @@ import Layout from '@/Components/Layout.vue';
 import { useForm } from '@inertiajs/vue3'
 
 const form = useForm({
-  image: null,
+    title: null,
+    location: null,
+    address: null,
+    country: null,
+    state: null,
+    city: null,
+    image: null,
+    concertDate: null,
+    band: null,
+    postImages: null,
+    text: null
 })
 
 
@@ -15,11 +25,60 @@ function submit(){
 
 <template>
     <Layout>
-        <div class="bg-white p-12">
+        <div class="bg-white p-8">
+            <p class="font-serif font-bold text-4xl mb-8">Share your experience with a new post!</p>
             <form @submit.prevent="submit">
-                <input type="file" @input="form.image = $event.target.files[0]" />
-                <img src="https://lalm-bucket.s3.amazonaws.com/Svct7YO0VxxFliyGe2jx4kd22RFZiNS8Xm6tXyO1.png?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAWXXDFBHVT2BQDPYR%2F20230622%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230622T193134Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Signature=4277018e7e63307225b13ffd5ae9dac528f9eaf0d18d9d5d478b4a8e33ecc52a" />
-                <button type="submit" class="ml-3 shadow-[-4px_4px_0px_0px_rgba(0,0,0,1)] border border-black px-2 hover:bg-black hover:text-white hover:border-l-white hover:border-b-white">Upload</button>
+                <div class="mb-2 flex">
+                    <label class="self-start" for="title">Title</label>
+                    <input class="ml-4" id="title"  type="text" v-model="form.title" />
+                </div>
+                <div class="mb-2 flex">
+                    <label class="self-start" for="location">Location</label>
+                    <input class="ml-4" id="location"  type="text" v-model="form.location" />
+                </div>
+                <div class="mb-2 flex">
+                    <label class="self-start" for="address">Address</label>
+                    <input class="ml-4" id="address" type="text"  v-model="form.address" />
+                </div>
+                <div class="mb-2 flex">
+                    <label class="self-start" for="country">Country</label>
+                    <input class="ml-4" id="country" type="text"  v-model="form.country" />
+                </div>
+                <div class="mb-2 flex">
+                    <label class="self-start" for="state">State</label>
+                    <input class="ml-4" id="state" type="text"  v-model="form.state" />
+                </div>
+                <div class="mb-4 flex">
+                    <label class="self-start" for="city">City</label>
+                    <input class="ml-4" id="city" type="text"  v-model="form.city" />
+                </div>
+                <div class="mb-6 flex">
+                    <label class="self-start" for="image">Main Image</label>
+                    <label for="image" class="ml-3 shadow-[-4px_4px_0px_0px_rgba(0,0,0,1)] border border-black px-4 py-2 hover:bg-black hover:text-white hover:border-l-white hover:border-b-white">
+                        Click here to add image!
+                    </label>
+                    <input id="image" class="hidden" type="file" @input="form.image = $event.target.files[0]" />
+                </div>
+                <div class="mb-4 flex">
+                    <label class="self-start" for="concertDate">Concert Date</label>
+                    <input class="ml-4" id="concertDate" type="date"  v-model="form.concertDate" />
+                </div>
+                <div class="mb-4 flex">
+                    <label class="self-start" for="band">Band Name</label>
+                    <input class="ml-4" id="band" type="text"  v-model="form.band" />
+                </div>
+                <div class="mb-5 flex">
+                    <label class="self-start" for="postImages">Gallery Images</label>
+                    <label for="postImages" class="ml-3 shadow-[-4px_4px_0px_0px_rgba(0,0,0,1)] border border-black px-4 py-2 hover:bg-black hover:text-white hover:border-l-white hover:border-b-white">
+                        Click here to add more images!
+                    </label>
+                    <input id="postImages" class="hidden" type="file" @input="form.image = $event.target.files[0]" />
+                </div>
+                <div class="mb-4 w-full">
+                    <label class="self-start" for="text">Share yourself!</label>
+                    <textarea class="ml-4 w-1/2" id="text"  v-model="form.text" rows="20" />
+                </div>
+                <button type="submit" class="w-1/3 ml-3 shadow-[-4px_4px_0px_0px_rgba(0,0,0,1)] border border-black px-4 py-2 hover:bg-black hover:text-white hover:border-l-white hover:border-b-white">Create Post!</button>
             </form>
         </div>
     </Layout>
