4797f684ca90261bc2b7bd576228de764af6f8bf
Marco Castillo <marcocastillo1997@gmail.com>
2023-06-27 00:44:53 -0600

    Adding margin to date title on post badge

--- resources/js/Components/PostBadge.vue
+++ resources/js/Components/PostBadge.vue
@@ -12,9 +12,9 @@
             <div class="p-4 flex justify-center flex-col">
                 <b class="font-serif text-center mb-3">{{ post.title }}</b>
                 <div class="font-sans">
-                    <p class="text-sm"><b>Lugar</b> {{ post.address }}</p>
-                    <p class="text-sm"><b>Fecha</b>{{ post.concert_date }} </p>
-                    <p class="text-sm"><b>Autor</b> {{ post.author.name }}</p>
+                    <p class="text-sm mr-1"><b>Lugar</b> {{ post.address }}</p>
+                    <p class="text-sm mr-1"><b>Fecha</b> {{ post.concert_date }} </p>
+                    <p class="text-sm mr-1"><b>Autor</b> {{ post.author.name }}</p>
                 </div>
             </div>
         </Link>
