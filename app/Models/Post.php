9fddf158ebedef7ee3c6850c8ce471e027278d54
Marco Castillo <marcocastillo1997@gmail.com>
2023-10-12 17:42:21 -0600

    Fix on getting image url from public folder

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,14 +16,14 @@ class Post extends Model
 
     protected $guarded = [];
 
-    protected function imageUrl(): Attribute
-    {
-        return Attribute::make(
-            get: fn (string $value) => Storage::temporaryUrl(
-                $value, now()->addMinutes(5)
-            )
-        );
-    }
+    // protected function imageUrl(): Attribute
+    // {
+    //     return Attribute::make(
+    //         get: fn (string $value) => Storage::temporaryUrl(
+    //             $value, now()->addMinutes(5)
+    //         )
+    //     );
+    // }
 
     public function tags(): BelongsToMany
     {
