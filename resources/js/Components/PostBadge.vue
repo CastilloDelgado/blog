d70780b6b3ccfd3fcc6dcf635d1ddd07c01d9cb5
Marco Castillo <marcocastillo1997@gmail.com>
2023-10-15 23:28:29 -0600

    Fix

--- resources/js/Components/PostBadge.vue
+++ resources/js/Components/PostBadge.vue
@@ -8,7 +8,7 @@ import BaseDate from './BaseDate.vue';
 </script>
 
 <template>
-    <div class="w-64 mb-6 bg-gray-300 shadow-[-10px_10px_0px_0px_rgba(0,0,0,1)] border-2 border-black " >
+    <div class="w-64 mb-6 bg-gray-300 shadow-[-10px_10px_0px_0px_rgba(0,0,0,1)] border-2 border-black" >
         <Link :href="route('posts.show', post)">
             <img :src="post.image_url" alt="" class="w-64 h-64 object-cover border-b-2 border-black">
             <div class="p-4 flex justify-center flex-col">
